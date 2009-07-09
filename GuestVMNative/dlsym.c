0e51e2e79612ddd80039f51d0d17c2e2369914f5
Mick Jordan <Mick.Jordan@sun.com>
2009-07-09 10:21:01 -0700

    Add GNU StrictMath and fdlibm/e_fmod

--- GuestVMNative/dlsym.c
+++ GuestVMNative/dlsym.c
@@ -1,24 +1,24 @@
 /*
  * Copyright (c) 2009 Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, California 95054, U.S.A. All rights reserved.
- * 
+ *
  * U.S. Government Rights - Commercial software. Government users are
  * subject to the Sun Microsystems, Inc. standard license agreement and
  * applicable provisions of the FAR and its supplements.
- * 
+ *
  * Use is subject to license terms.
- * 
+ *
  * This distribution may include materials developed by third parties.
- * 
+ *
  * Parts of the product may be derived from Berkeley BSD systems,
  * licensed from the University of California. UNIX is a registered
  * trademark in the U.S.  and in other countries, exclusively licensed
  * through X/Open Company, Ltd.
- * 
+ *
  * Sun, Sun Microsystems, the Sun logo and Java are trademarks or
  * registered trademarks of Sun Microsystems, Inc. in the U.S. and other
  * countries.
- * 
+ *
  * This product is covered and controlled by U.S. Export Control laws and
  * may be subject to the export or import laws in other
  * countries. Nuclear, missile, chemical biological weapons or nuclear
@@ -27,7 +27,7 @@
  * U.S. embargo or to entities identified on U.S. export exclusion lists,
  * including, but not limited to, the denied persons and specially
  * designated nationals lists is strictly prohibited.
- * 
+ *
  */
 /*
  * Maxine assumes that native symbols are resolved by the dlsym function, which is normally
@@ -68,7 +68,6 @@ extern void * blk_dlsym(const char *symbol);
 extern void * thread_stack_pool_dlsym(const char *symbol);
 extern void * heap_pool_dlsym(const char *symbol);
 extern void * code_pool_dlsym(const char *symbol);
-extern void * StrictMath_dlsym(const char *symbol);
 extern void * inflater_dlsym(const char *symbol);
 
 
@@ -81,8 +80,7 @@ void *dlsym(void *a1, const char *symbol) {
       (result = blk_dlsym(symbol)) ||
       (result = thread_stack_pool_dlsym(symbol)) ||
       (result = heap_pool_dlsym(symbol)) ||
-      (result = code_pool_dlsym(symbol)) ||
-      (result = StrictMath_dlsym(symbol))) {
+      (result = code_pool_dlsym(symbol))) {
     return result;
   } else {
     guk_printk("Guest VM: symbol %s not found, exiting\n", symbol);
