2091d73e519ea4f24a9ff57c750a440476bb9ffe
Mick Jordan <Mick.Jordan@sun.com>
2010-06-16 08:54:03 -0700

    add static spinlock methods

--- GuestVM/src/com/sun/guestvm/spinlock/guk/GUKSpinLock.java
+++ GuestVM/src/com/sun/guestvm/spinlock/guk/GUKSpinLock.java
@@ -1,24 +1,24 @@
 /*
  * Copyright (c) 2009 Sun Microsystems, Inc., 4150 Network Circle, Santa
  * Clara, California 95054, U.S.A. All rights reserved.
- * 
+ *
  * U.S. Government Rights - Commercial software. Government users are
  * subject to the Sun Microsystems, Inc. standard license agreement and
  * applicable provisions of the FAR and its supplements.
- * 
+ *
  * Use is subject to license terms.
- * 
+ *
  * This distribution may include materials developed by third parties.
- * 
+ *
  * Parts of the product may be derived from Berkeley BSD systems,
  * licensed from the University of California. UNIX is a registered
  * trademark in the U.S.  and in other countries, exclusively licensed
  * through X/Open Company, Ltd.
- * 
+ *
  * Sun, Sun Microsystems, the Sun logo and Java are trademarks or
  * registered trademarks of Sun Microsystems, Inc. in the U.S. and other
  * countries.
- * 
+ *
  * This product is covered and controlled by U.S. Export Control laws and
  * may be subject to the export or import laws in other
  * countries. Nuclear, missile, chemical biological weapons or nuclear
@@ -27,7 +27,7 @@
  * U.S. embargo or to entities identified on U.S. export exclusion lists,
  * including, but not limited to, the denied persons and specially
  * designated nationals lists is strictly prohibited.
- * 
+ *
  */
 package com.sun.guestvm.spinlock.guk;
 
@@ -92,12 +92,30 @@ public class GUKSpinLock extends SpinLock {
 
     @Override
     public void lock() {
-        GUKScheduler.spinLock(_native._spinlock);
+        GUKSpinLock.spinLock(_native._spinlock);
     }
 
     @Override
     public void unlock() {
-        GUKScheduler.spinUnlock(_native._spinlock);
+        GUKSpinLock.spinUnlock(_native._spinlock);
+    }
+
+    /**
+     * Release spin lock created elsewhere.
+     * @param lock
+     */
+    @INLINE
+    public static void spinUnlock(Pointer lock) {
+        GUK.guk_spin_unlock(lock);
+    }
+
+    /**
+     * Acquire lock created elsewhere.
+     * @param lock
+     */
+    @INLINE
+    public static void spinLock(Pointer lock) {
+        GUK.guk_spin_lock(lock);
     }
 
 }
