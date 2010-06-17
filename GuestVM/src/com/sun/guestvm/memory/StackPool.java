3555b8cbb0ae1711e51e429d5b75ea2da6feb7be
Mick Jordan <Mick.Jordan@sun.com>
2010-06-17 10:23:05 -0700

    move stack validation to test program

--- GuestVM/src/com/sun/guestvm/memory/StackPool.java
+++ GuestVM/src/com/sun/guestvm/memory/StackPool.java
@@ -32,13 +32,8 @@
 package com.sun.guestvm.memory;
 
 import com.sun.guestvm.guk.GUKBitMap;
-import com.sun.guestvm.guk.GUKPageTables;
-import com.sun.guestvm.guk.x64.*;
-import com.sun.guestvm.spinlock.guk.*;
 import com.sun.max.annotate.*;
 import com.sun.max.unsafe.*;
-
-import static com.sun.guestvm.guk.GUKPageTables.*;
 
 /**
  * This is an interface to the pool of virtual memory that is used for thread stacks.
@@ -69,94 +64,8 @@ public class StackPool {
         return GUKBitMap.isAllocated(_bitMap, slot);
     }
 
-    /**
-     * Validates the page frame mappings of all the thread stacks in the pool.
-     */
-    public static void validate() {
-        final Pointer lock = guestvmXen_stackPoolSpinLock();
-        GUKSpinLock.spinLock(lock);
-        for (int i = 0; i < getSize(); i++) {
-            if (isAllocated(i)) {
-                GUKSpinLock.spinUnlock(lock);
-                validateStack(i);
-                GUKSpinLock.spinLock(lock);
-            }
-        }
-        GUKSpinLock.spinUnlock(lock);
-    }
-
-    /**
-     * A Java thread stack should have the following format:
-     *
-     * Red zone page: unmapped
-     * Yellow zone page: mapped but not present
-     * Zero or more unmapped pages
-     * Blue zone page: mapped but not present
-     * 7 or more active pages: mapped and present
-     *
-     * The VM thinks the stack base is at the start of the Yellow zone page.
-     * The information on where the blue zone page is currently located is stored
-     * in the NativeThreadLocalsStruct, {@link NativeThreadLocal}, but that
-     * is not accessible from the stack pool.
-     */
-    private static void validateStack(int i) {
-        final long regionSize = getRegionSize();
-        final long stackBase = getBase().toLong() + i * regionSize;
-        final long stackEnd = stackBase + regionSize;
-        boolean invalid = false;
-        long p = stackBase;
-        System.out.println("Validating stack page frames for slot " + i);
-        check(p, pageStatus(p), PageStatus.UNMAPPED);
-        p += X64VM.PAGE_SIZE;
-        check(p, pageStatus(p), PageStatus.MAPPED_NOT_PRESENT);
-        p += X64VM.PAGE_SIZE;
-        while (p < stackEnd) {
-            PageStatus ps = pageStatus(p);
-            while (p < stackEnd && ps == PageStatus.UNMAPPED) {
-                p += X64VM.PAGE_SIZE;
-                ps = pageStatus(p);
-            }
-            if (p >= stackEnd) {
-                // never found any mapped pages
-                System.out.println("no active pages found");
-                invalid = true;
-                break;
-            }
-            check(p, ps, PageStatus.MAPPED_NOT_PRESENT);
-            p += X64VM.PAGE_SIZE;
-            ps = pageStatus(p);
-            while (p < stackEnd && ps == PageStatus.MAPPED_PRESENT) {
-                p += X64VM.PAGE_SIZE;
-                ps = pageStatus(p);
-            }
-            if (p < stackEnd) {
-                System.out.println("unexpected page state " + ps + ", at " + Long.toHexString(p));
-                invalid = true;
-                break;
-            }
-        }
-    }
-
-
-    private enum PageStatus {
-        MAPPED_PRESENT,
-        MAPPED_NOT_PRESENT,
-        UNMAPPED
-    }
-
-    private static PageStatus pageStatus(long p) {
-        try {
-            GUKPageTables.getPteForAddress(Address.fromLong(p));
-            return PageStatus.MAPPED_PRESENT;
-        } catch (PteNotPresentException ex) {
-            return ex._pte == 0 ? PageStatus.UNMAPPED : PageStatus.MAPPED_NOT_PRESENT;
-        }
-    }
-
-    private static void check(long p, PageStatus real, PageStatus desired) {
-        if (real != desired) {
-            System.out.println("page at address " + Long.toHexString(p) + " is " + real + ", should be " + desired);
-        }
+    public static Pointer getSpinLock() {
+        return guestvmXen_stackPoolSpinLock();
     }
 
     @CONSTANT_WHEN_NOT_ZERO
