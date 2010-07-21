21b43fc17eeee062cbbaedd67170d0f9b4060aef
Mick Jordan <Mick.Jordan@sun.com>
2010-07-21 15:59:15 -0700

    tick-based profiler

--- GuestVM/src/com/sun/guestvm/profiler/Tick.java
+++ GuestVM/src/com/sun/guestvm/profiler/Tick.java
@@ -1,15 +1,24 @@
 package com.sun.guestvm.profiler;
 
 import java.util.*;
+
+import com.sun.max.vm.compiler.target.*;
+import com.sun.max.vm.*;
+import com.sun.max.vm.runtime.*;
+import com.sun.max.vm.stack.*;
+import com.sun.max.vm.thread.VmThread;
+import com.sun.max.unsafe.*;
+
 /**
  * Tick profiler. Runs a thread that periodically wakes up, stops all the threads, and records their state.
  *
  * @author Mick Jordan
  *
  */
-public class Tick implements Runnable {
+public class Tick extends Thread {
 
-    private Random rand;
+    private static final int DEFAULT_FREQUENCY = 50;
+    private static final Random rand = new Random();;
     private int frequency;
     private int jiggle;
 
@@ -17,9 +26,15 @@ public class Tick implements Runnable {
      * Create a tick profiler with given frequency.
      * @param frequency
      */
-    Tick(int frequency) {
-        this.frequency = frequency;
-        jiggle = frequency / 8;
+    private Tick(int frequency) {
+        this.frequency = frequency == 0 ? DEFAULT_FREQUENCY : frequency;
+        jiggle = this.frequency / 8;
+        setName("Tick-Profiler");
+        setDaemon(true);
+     }
+
+    public static void create(int frequency) {
+        new Tick(frequency).start();
     }
 
     public void run() {
@@ -28,11 +43,40 @@ public class Tick implements Runnable {
                 final int thisJiggle = rand.nextInt(jiggle);
                 final int thisPeriod = frequency + (rand.nextBoolean() ? thisJiggle : -thisJiggle);
                 Thread.sleep(thisPeriod);
-
+                final long start = System.nanoTime();
+                stackTraceGatherer.run();
+                final long end = System.nanoTime();
+                Log.print("profile at: "); Log.print(start); Log.print(", duration "); Log.println(end - start);
             } catch (InterruptedException ex) {
             }
-
         }
     }
 
+    private static final class StackTraceGatherer extends StopThreadsForOperation {
+        StackTraceGatherer(Pointer.Predicate p) {
+            super(p);
+        }
+        @Override
+        public void perform(Pointer threadLocals, Pointer ip, Pointer sp, Pointer fp) {
+            final VmStackFrameWalker stackFrameWalker = VmThread.fromVmThreadLocals(threadLocals).stackDumpStackFrameWalker();
+            stackFrameWalker.inspect(ip, sp, fp, stackFrameDumper);
+        }
+    }
+
+    private static final StackTraceGatherer stackTraceGatherer = new StackTraceGatherer(new IsNotCurrentThread());
+
+    public static final class IsNotCurrentThread implements Pointer.Predicate {
+
+        public boolean evaluate(Pointer vmThreadLocals) {
+            return vmThreadLocals != VmThread.current().vmThreadLocals();
+        }
+    }
+
+    private static final FrameDumper stackFrameDumper = new FrameDumper();
+
+    private static class FrameDumper implements RawStackFrameVisitor {
+        public boolean visitFrame(TargetMethod targetMethod, Pointer ip, Pointer sp, Pointer fp, boolean isTopFrame) {
+            return true;
+        }
+    }
 }
